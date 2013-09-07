8c7d7f5fa2f72404f7e85f39895d7980b48dbc68
Jan Vermeulen <github@entropy.co.za>
2013-09-07 15:28:51 +0200

    Now outputs access period with overall validity

--- rollovermax.rb
+++ rollovermax.rb
@@ -2,8 +2,8 @@ require 'date'
 
 recur = 30 #days after which a new bundle is allocated
 term = 12 #months after the start date you will get data 
-start_date = Date.new(2012,8,7) #when the promotion starts
-end_date = Date.new(2013,2,11) #when the promotion ends
+start_date = Date.new(2012,8,1) #when the promotion starts
+end_date = Date.new(2013,2,6) #when the promotion ends
 
 max_validity = 0 #sum of the validity periods of the bundles over the whole term
 optimal_dates = [] #the start dates that give you max_validity
@@ -11,11 +11,13 @@ optimal_dates = [] #the start dates that give you max_validity
 start_date.step(end_date, step=1) do |date|
 	bundle_start = date
 	validity_total = 0
+	bundle_end = Date.new((date + term * recur >> 2).year, (date + term * recur >> 2).mon, 1) - 1 #initial value not important
 	term.times do |n| # can't figure out how to use Date.step for this
 		bundle_end = Date.new((bundle_start >> 2).year, (bundle_start >> 2).mon, 1) - 1
 		validity_total += (bundle_end - bundle_start).to_i
 		bundle_start += recur
 	end
+	puts "#{date} – #{bundle_end} (#{date + (term - 1) * recur}): #{validity_total}, #{(bundle_end - date).to_i}"
 	if validity_total > max_validity
 		max_validity = validity_total
 		optimal_dates = [date]
@@ -26,6 +28,5 @@ start_date.step(end_date, step=1) do |date|
 	end
 end
 
-puts "Best validity period: #{max_validity}"
-puts "Optimal start date(s):"
+puts "Best validity period: #{max_validity}, on:"
 puts optimal_dates
